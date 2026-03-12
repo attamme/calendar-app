c85b4686c6332e12d5b91946be736f5f5b6a8937
Mats Saaren <mats.saaren@voco.ee>
2026-03-12 13:46:18 +0200

    textinput base

---
+++ frontend/components/InputText.tsx
@@ -0,0 +1,17 @@
+import { Pressable, TextInput } from "react-native";
+import { styles } from "@/styles/input_text";
+import { useRef } from "react";
+
+type Props = {
+    placeholder: string;
+}
+
+export default function InputText ({ placeholder } : Props ) {
+    const inputRef = useRef<TextInput>(null);
+
+    return (
+        <Pressable style={styles.inputText} onPress={() => inputRef.current?.focus()}>
+            <TextInput placeholder={placeholder} style={styles.text} ref={inputRef} />
+        </Pressable>
+    )
+}
---
+++ frontend/styles/input_text.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    inputText: {
+        height: 48,
+        backgroundColor: "#50577a",
+        alignItems: "center",
+        paddingHorizontal: 138,
+        paddingVertical: 14,
+        gap: 10,
+        borderRadius: 100
+    },
+    text: {
+        height: 18,
+        width: "100%",
+        position: "absolute",
+        top: 15,
+        left: 19,
+        fontSize: 15,
+        fontFamily: "Inter-Regular",
+        color: "#eee",
+    }
+})
