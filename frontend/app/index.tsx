968917c8ded1c57f8922c57ecd4423b5f7aee4e9
Mats Saaren <mats.saaren@voco.ee>
2026-03-16 12:42:13 +0200

    keyboard part seems fixed

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,13 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View, Keyboard, KeyboardAvoidingView, Platform, Pressable } from "react-native";
 import InputText from "@/components/InputText";
 import { styles } from "@/styles/test";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <InputText label="Text" placeholder="Text"/>
-      <InputText label="Password" placeholder="**********" secure />
-    </View>
+
+    <KeyboardAvoidingView style={{ flex: 1 }} behaviour={Platform.OS === "ios" ? "padding" : "height"}>
+      <Pressable style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+          <InputText label="Text" placeholder="Text"/>
+          <InputText label="Password" placeholder="**********" secure />
+        </View>
+      </Pressable>
+    </KeyboardAvoidingView>
   );
 }
