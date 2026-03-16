dcffddbb00534e517606ffd3872b4dc952e0d3eb
M2LTN <aaron.malton@voco.ee>
2026-03-16 11:45:47 +0200

    Added logo component

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,16 +1,10 @@
 import { Text, View } from "react-native";
-import Button from "@/components/button";
+import Logo from '@/components/logo';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button title="Button"/>
+    <View>
+      <Logo/>
     </View>
   );
 };
