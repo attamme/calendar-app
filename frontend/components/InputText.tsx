3958c704c73b8de2d37cb89618c29a936c9290b9
Mats Saaren <mats.saaren@voco.ee>
2026-03-19 08:52:36 +0200

    issue #8 added onchangetext (ask mats)

--- frontend/components/InputText.tsx
+++ frontend/components/InputText.tsx
@@ -10,6 +10,7 @@ type Props = {
     placeholder: string;
     value?: string;
     secure?: boolean;
+    onChangeText?: () => void;
 }
 
 export default function InputText ({ label, placeholder, value, secure, onChangeText } : Props ) {
