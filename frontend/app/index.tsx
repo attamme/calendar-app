40536b549db23589475548e1a4253a95710db01d
M2LTN <aaron.malton@voco.ee>
2026-03-25 15:00:47 +0200

    Created TaskView component

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,20 +2,12 @@ import { Text, View, Keyboard, KeyboardAvoidingView, Platform, Pressable } from 
 import { useRouter } from "expo-router";
 import Button from "@/components/button";
 import InputText from "@/components/InputText";
+import TaskView from "@/components/TaskView";
 
 export default function Index() {
   const router = useRouter()
   return (
+    <TaskView />
+    );
 
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : "height"}>
-      <Pressable style={{ flex: 1 }} onPress={Keyboard.dismiss}>
-        <View>
-          <Text>Edit app/index.tsx to edit this screen.</Text>
-          <InputText label="Text" placeholder="Text"/>
-          <InputText label="Password" placeholder="**********" secure />
-          <Button title="Login" onPress={ () => router.navigate("/(auth)/login")}/>
-        </View>
-      </Pressable>
-    </KeyboardAvoidingView>
-  );
-};
+  };
