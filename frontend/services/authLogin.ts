d6797a0acd4a015d856d21632cf587cd3796d9dd
Mats Saaren <mats.saaren@voco.ee>
2026-04-08 09:51:50 +0300

    Update login function to use 'email' parameter and correct API endpoint

--- frontend/services/authLogin.ts
+++ frontend/services/authLogin.ts
@@ -1,15 +1,16 @@
 import { saveToken } from "@/services/authStorage";
 import {API_URL} from "@/app/config.json";
 
-export default async function login(username: string, password: string) {
-  console.log(API_URL + "/login")
-  const res = await fetch(`${API_URL}/login`, {
+export default async function login(email: string, password: string) {
+  console.log(API_URL + "/users/login")
+  const res = await fetch(`${API_URL}/users/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "69",
       
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
   });
   console.log(res);
   if (!res.ok) {
